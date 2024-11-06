a2cac1686060fafd2c9f4cc1c4d9068f54c95c36
khaled <khaleddouadi85@gmail.com>
2024-11-06 21:09:55 +0100

    mzl partie 2

--- partie_3/lab8/testmax.c
+++ partie_3/lab8/testmax.c
@@ -5,20 +5,20 @@
 
 int main() {
     // Créer et initialiser un tableau de 5 éléments
-    int arr[5] = {2, 5, 7, 2, 9};
+    int arr[10] = {4 , 9 , 10 , 11 , 12 , 15 , 12 , 9 , 0 , 30};
     int max_value; // Variable pour stocker la valeur maximale
     int min_value;
     float var = 0;
 
 
     // Appeler la fonction max
-    maxim(arr, 5, &max_value); // Passer l'adresse de max_value
+    maxim(arr, 10, &max_value); // Passer l'adresse de max_value
 
     // Appeler la fonction min
-    min(arr, 5, &min_value); // Passer l'adresse de max_value
+    min(arr, 10, &min_value); // Passer l'adresse de max_value
 
     // Appeler la fonction var
-    variance(arr, 5, &var); // Passer l'adresse de max_value
+    variance(arr, 10, &var); // Passer l'adresse de max_value
 
     // Afficher le résultat
     printf("La valeur maximale du tableau est : %d\n La valeur minimale du tableau est : %d\n La variance du tableau est : %f\n", max_value , min_value, var);
